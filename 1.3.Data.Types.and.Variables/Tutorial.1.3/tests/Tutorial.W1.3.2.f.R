37818caabc9a0765f77e7a75f6b35b786556b480
Heather Gibling <heathergibling@gmail.com>
2022-02-01 22:14:37 -0500

    update files

--- 1.3.Data.Types.and.Variables/Tutorial.1.3/tests/Tutorial.W1.3.2.f.R
+++ 1.3.Data.Types.and.Variables/Tutorial.1.3/tests/Tutorial.W1.3.2.f.R
@@ -6,7 +6,8 @@ test = list(
       name = NA,
       points = 1.0,
       code = {
-        expect_equal(doctors_note, "Patient complains of 'bloating pains'. Laparoscopicsurgery scheduled for Oct. 10, 2021.")
+        # accept both double or single quotes around "bloating pains"
+        expect_true(doctors_note %in% c("Patient complains of 'bloating pains'. Laparoscopicsurgery scheduled for Oct. 10, 2021.", 'Patient complains of "bloating pains". Laparoscopicsurgery scheduled for Oct. 10, 2021.'))
       }
     )
   )
